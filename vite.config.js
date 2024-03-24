0548b3dceeaca13f5e3091b8106b861b0a798ff5
Ankush Ranapure <ar2653@njit.edu>
2024-03-23 23:24:08 -0400

    Added docker file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: './',
-})
+ base: "/",
+ plugins: [vue()],
+ preview: {
+  port: 8080,
+  strictPort: true,
+ },
+ server: {
+  port: 8080,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:8080",
+ },
+});
